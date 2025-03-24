cadf73ef0ed7477f6d44f5c9382e168762535a8e
Brayanketchup <brayanmartinez1102@gmail.com>
2025-03-23 20:05:33 -0400

    debuggin

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default async function Home({ searchParams }: HomeProps) {
           </section>
         </div>
         <div className='w-full h-auto py-16 flex flex-col text-center text-white bg-[#2d2d2d] '>
-          <h1 className=' font-extrabold 2xl:text-[52px] sm:text-[44px] text-[40px] '>Save big with our cheap car rental!</h1>
+          <h1 className=' font-extrabold 2xl:text-[52px] sm:text-[44px] text-[40px] '>Save big with our cheap car rental!!</h1>
           <p className='text-[27px]'>Top Airports. Local Suppliers. <span className="text-primary-color">24/7</span> Support.</p>
         </div>
         
